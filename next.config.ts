65597c3e34fd777265d5d062f58b0f596c45654b
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-04 13:12:01 +0530

    Add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,43 @@
+
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: false, // Important: this allows us to show the update prompt
+  disable: process.env.NODE_ENV === 'development',
+  
+})
+
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i0.wp.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.papihairdesign.sk',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+module.exports = withPWA(nextConfig);
